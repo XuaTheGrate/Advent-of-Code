bc96889f0805c971a77bcd886a88c355a562ca79
Maya <17090652+XuaTheGrate@users.noreply.github.com>
2021-12-07 18:18:27 +1300

    day 7

--- src/main/kotlin/aoc/Day.kt
+++ src/main/kotlin/aoc/Day.kt
@@ -5,14 +5,16 @@ import java.io.File
 abstract class Day {
     abstract val dayNum: Int
     abstract val year: Int
+
+    open val rawData: String by lazy {
+        File("data/$year/day$dayNum.txt").readText(Charsets.UTF_8)
+    }
+
     open val data: List<String> by lazy {
         rawData
             .trim()
             .split('\n')
             .map(String::trim)
-    }
-    open val rawData: String by lazy {
-        File("data/$year/day$dayNum.txt").readText(Charsets.UTF_8)
     }
 
     abstract fun solutionPart1(): Number
--- src/main/kotlin/aoc/xxi/Day7.kt
+++ src/main/kotlin/aoc/xxi/Day7.kt
@@ -1,10 +1,29 @@
 package aoc.xxi
         
-import aoc.unreachable
-        
+import kotlin.math.absoluteValue
+
 class Day7: DayXXI() {
     override val dayNum = 7
     
-    override fun solutionPart1(): Number = unreachable()
-    override fun solutionPart2(): Number = unreachable()
+    override fun solutionPart1(): Number {
+        val data = data.first().split(",").map(String::toInt)
+        val fuels = mutableListOf<Int>()
+        for (i in 0 .. data.maxOrNull()!!) {
+            fuels.add(data.sumOf { (i - it).absoluteValue })
+        }
+        return fuels.minOrNull()!!
+    }
+
+    override fun solutionPart2(): Number {
+        val data = data.first().split(",").map(String::toInt)
+        val fuels = mutableListOf<Int>()
+        for (i in 0 .. data.maxOrNull()!!) {
+            fuels.add(data.sumOf {
+                val a = (i - it).absoluteValue
+                val b = ((i - it).absoluteValue + 1)
+                (a * b) / 2
+            })
+        }
+        return fuels.minOrNull()!!
+    }
 }
